d643860919dd766499629bc5cac595c4b779d6d6
Jon Goldson <forjrg@gmail.com>
2023-03-30 08:02:41 -0700

    editing share stuff on document

--- pages/_document.js
+++ pages/_document.js
@@ -4,15 +4,15 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="Daily Workout" key="title" />
+        <meta property="og:title" content="LoLGPT" key="title" />
         <meta
           property="og:description"
-          content="Custom AI Workouts"
+          content="League Of Legends AI powered by OpenAI"
           key="description"
         />
         <meta
           property="og:image"
-          content="https://i.ibb.co/q5n2Hjx/dailyworkout.jpg"
+          content="https://mcdn.wallpapersafari.com/medium/68/60/FPLiZR.jpg"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
         <link
